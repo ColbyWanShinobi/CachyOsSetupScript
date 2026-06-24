c28116a2aa21bf6bca81b9b89cff06ce664e1c44
Colby Butler <colby@owljet.com>
2026-06-24 00:31:14 -0400

    fix

--- install-plasma.sh
+++ install-plasma.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+sudo pacman -Syu
 sudo pacman -Syu plasma-meta kde-applications-meta cachyos-kde-settings sddm
 sudo systemctl disable gdm.service
 sudo systemctl enable sddm.service
